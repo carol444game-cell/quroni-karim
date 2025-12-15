c13b2cf528cf77e85a7b3461a8f3d746e0640ebf
carol444game-cell <carol444game@gmail.com>
2025-12-15 21:27:58 +0500

    Update models.py

--- models.py
+++ models.py
@@ -5,24 +5,15 @@ db = SQLAlchemy()
 
 class Ayah(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
-    ayah_uid = db.Column(db.String(150), unique=True, nullable=False)
-    sura = db.Column(db.String(100), nullable=False)
-    ayah_number = db.Column(db.String(20), nullable=False)
-
-    text = db.Column(db.Text)
-    audio_file_id = db.Column(db.String(255))
-
-    channel_id = db.Column(db.String(50))
-    message_id = db.Column(db.String(50))
-
+    ayah_id = db.Column(db.String(50), unique=True, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    audio_file_id = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def to_dict(self):
-        return {
-            "uid": self.ayah_uid,
-            "sura": self.sura,
-            "ayah": self.ayah_number,
-            "text": self.text,
-            "audio": self.audio_file_id
-        }
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.BigInteger, unique=True, nullable=False)
+    username = db.Column(db.String(50))
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    joined_at = db.Column(db.DateTime, default=datetime.utcnow)
